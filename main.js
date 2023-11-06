b08fbb3c6aa1bc3e3319b9096071ca1d0604ed20
Q210155 <l.loiseau@student.helmo.be>
2023-11-06 22:23:10 +0100

    Merge

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ gsap.set('.cursor', {xPercent: -50, yPercent: -50});
 
 
 const follow = document.querySelector('.cursor');
-const cursor = document.querySelector('.follower');
 
 window.addEventListener('mousemove', e => {
-    gsap.to(follow, 0.2, { x: e.clientX, y: e.clientY });
-    gsap.to(cursor, 0.9, { x: e.clientX, y: e.clientY});
+    gsap.to(follow, 0.4, { x: e.clientX, y: e.clientY });
 });
