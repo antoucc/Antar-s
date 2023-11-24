4fb00a773c842f03db0cef94f06a510993675de7
Q210155 <l.loiseau@student.helmo.be>
2023-11-24 23:59:56 +0100

    Merge

--- scripts/methodology-section.js
+++ scripts/methodology-section.js
@@ -1,33 +1,71 @@
-const steps = document.querySelectorAll('.step');
-const canvas = document.getElementById('line');
-const ctx = canvas.getContext('2d');
+const lineProgress = document.getElementById('line-progress');
 
-updateCanvas(document.getElementById('line'), steps);
+document.querySelectorAll('.step').forEach(function(step) {
+    step.addEventListener('click', function() {
+        this.classList.add('active');
+        let previousSiblings = getPreviousSiblings(this);
+        previousSiblings.forEach(sib => sib.classList.add('active'));
 
-function updateCanvas(canvas, stepsElements) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.strokeStyle = '#fff';
-    ctx.fillStyle = '#fff';
-    ctx.lineWidth = 2;
-
-    const lineY = canvas.height / 2;
-
-    // Draw the line
-    ctx.beginPath();
-    ctx.moveTo(0, lineY);
-    ctx.lineTo(canvas.width, lineY);
-    ctx.stroke();
-
-
-    const spacing = canvas.width / (stepsElements.length + 1);
-
-    stepsElements.forEach((step, index) => {
-        const dotX = spacing * (index + 1);
-        const dotRadius = 5;
-
-        ctx.beginPath();
-        ctx.arc(dotX, lineY, dotRadius, 0, Math.PI * 2, true);
-        ctx.fill();
+        let nextSiblings = getNextSiblings(this);
+        nextSiblings.forEach(sib => sib.classList.remove('active'));
     });
+});
+
+document.querySelector('.step01').addEventListener('click', function() {
+    document.getElementById('line-progress').style.width = '3%';
+    setActiveClass(document.querySelector('.discovery'));
+});
+
+document.querySelector('.step02').addEventListener('click', function() {
+    document.getElementById('line-progress').style.width = '25%';
+    setActiveClass(document.querySelector('.strategy'));
+});
+
+document.querySelector('.step03').addEventListener('click', function() {
+    document.getElementById('line-progress').style.width = '50%';
+    setActiveClass(document.querySelector('.creative'));
+});
+
+document.querySelector('.step04').addEventListener('click', function() {
+    document.getElementById('line-progress').style.width = '75%';
+    setActiveClass(document.querySelector('.production'));
+});
+
+document.querySelector('.step05').addEventListener('click', function() {
+    document.getElementById('line-progress').style.width = '100%';
+    setActiveClass(document.querySelector('.analysis'));
+});
+
+function setActiveClass(element) {
+    element.classList.add('active');
+    let siblings = getSiblings(element);
+    siblings.forEach(sib => sib.classList.remove('active'));
+}
+
+function getSiblings(element) {
+    let siblings = [];
+    let sibling = element.parentNode.firstChild;
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== element) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+    return siblings;
+}
+
+function getPreviousSiblings(element) {
+    let siblings = [];
+    while (element === element.previousElementSibling) {
+        siblings.push(element);
+    }
+    return siblings;
+}
+
+function getNextSiblings(element) {
+    let siblings = [];
+    while (element === element.nextElementSibling) {
+        siblings.push(element);
+    }
+    return siblings;
 }
